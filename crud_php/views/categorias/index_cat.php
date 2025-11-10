18105053e47b10165286882d90b1d331934e4a19
Leonardo <leokenjits@gmail.com>
2025-11-10 11:26:22 -0300

    Add comments for course and authors in index_cat.php
    Added course information and author details as comments.

--- crud_php/views/categorias/index_cat.php
+++ crud_php/views/categorias/index_cat.php
@@ -1,5 +1,16 @@
 <?php
 include_once __DIR__ . '/../../controllers/CategoriaController.php';
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 10 de novembro de 2025
+*******************************************************************************/
 
 $controller = new CategoriaController();
 $data = $controller->index();
