37c04f3b7933ce847d1ce763f8d5c768ac07a7c7
Leonardo <leokenjits@gmail.com>
2025-11-10 11:23:15 -0300

    Add header comments to Categoria.php
    Added header comments with course and team details.

--- crud_php/models/Categoria.php
+++ crud_php/models/Categoria.php
@@ -1,4 +1,17 @@
 <?php
+
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: José Carlos Domingues Flôres
+Turma: ESOFT-2A
+Componentes:
+ 25185655-2 - Leonardo Kenji Tanida Soares
+ 25125961-2 - Elias Borgers Neckel
+ 25011023-2 - Lucas Coelho Suero 
+Data: 10 de novembro de 2025
+*******************************************************************************/
+
 class Categoria {
     private $conn;
     private $table_name = "categorias";
